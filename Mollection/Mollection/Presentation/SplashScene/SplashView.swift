2ec1f861bfab706ccec6589ec2da9d935c3aca35
wodyddml2 <wodyddml4@naver.com>
2023-02-09 17:57:01 +0900

    Feat: SplashView 임시 로고 생성

--- Mollection/Mollection/Presentation/SplashScene/SplashView.swift
+++ Mollection/Mollection/Presentation/SplashScene/SplashView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct SplashView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Image(systemName: "play.fill")
+            .foregroundColor(.customPurple)
+            .font(.system(size: 130))
     }
 }
 
