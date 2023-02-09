c4c4b5e2d46e7dcf7128f3b03a8837eeef5cda33
wodyddml2 <wodyddml4@naver.com>
2023-02-09 16:58:35 +0900

    git init

---
+++ Mollection/Mollection/MollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MollectionApp.swift
+//  Mollection
+//
+//  Created by J on 2023/02/09.
+//
+
+import SwiftUI
+
+@main
+struct MollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
