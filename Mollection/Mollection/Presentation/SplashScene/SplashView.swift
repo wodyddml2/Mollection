dad3e3fc5e355a38d0148a652d6c51d899d399eb
wodyddml2 <wodyddml4@naver.com>
2023-02-09 17:45:26 +0900

    Feat: SplashView 생성

---
+++ Mollection/Mollection/Presentation/SplashScene/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  Mollection
+//
+//  Created by J on 2023/02/09.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SplashView_Previews: PreviewProvider {
+    static var previews: some View {
+        SplashView()
+    }
+}
