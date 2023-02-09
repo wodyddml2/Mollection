1cec8e0eb3422e8f49adb8566b47c08e93803ac4
wodyddml2 <wodyddml4@naver.com>
2023-02-09 18:00:40 +0900

    Feat: PropertyWrapper 생성 - UserDefaults 관리

---
+++ Mollection/Mollection/Utility/PropertyWrapper/UserDefault.swift
@@ -0,0 +1,31 @@
+//
+//  UserDefault.swift
+//  Mollection
+//
+//  Created by J on 2023/02/09.
+//
+
+import Foundation
+
+@propertyWrapper
+struct UserDefault<T> {
+    let key: String
+    let defaultValue: T
+    let storages: UserDefaults
+    
+    var wrappedValue: T {
+        get { self.storages.object(forKey: self.key) as? T ?? self.defaultValue }
+        set { self.storages.set(newValue, forKey: self.key)}
+    }
+    
+    init(key: String, defaultValue: T, storages: UserDefaults = .standard) {
+        self.key = key
+        self.defaultValue = defaultValue
+        self.storages = storages
+    }
+}
+
+class UserManager {
+    @UserDefault(key: "login", defaultValue: false)
+    static var login: Bool
+}
