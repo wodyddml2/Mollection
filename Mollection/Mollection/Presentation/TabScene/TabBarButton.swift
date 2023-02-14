a6613eda03b95175853f31fc955a223e4252ef1f
wodyddml2 <wodyddml4@naver.com>
2023-02-14 14:11:10 +0900

    Feat: Custom TabBarButton 파일 생성

---
+++ Mollection/Mollection/Presentation/TabScene/TabBarButton.swift
@@ -0,0 +1,22 @@
+//
+//  TabBarButton.swift
+//  Mollection
+//
+//  Created by J on 2023/02/14.
+//
+
+import SwiftUI
+
+struct TabBarButton: View {
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TabBarButton_Previews: PreviewProvider {
+    static var previews: some View {
+        TabBarButton()
+    }
+}
