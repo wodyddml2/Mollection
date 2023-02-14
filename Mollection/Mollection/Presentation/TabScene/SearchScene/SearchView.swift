a9e6504f1d52febcf35cb066fa1900b52afa548a
wodyddml2 <wodyddml4@naver.com>
2023-02-14 17:03:39 +0900

    Feat: SearchView UI 틀 구현

--- Mollection/Mollection/Presentation/TabScene/SearchScene/SearchView.swift
+++ Mollection/Mollection/Presentation/TabScene/SearchScene/SearchView.swift
@@ -12,8 +12,39 @@ struct SearchView: View {
     
     var body: some View {
         List {
-            Text("A")
+            HStack(alignment: .top) {
+                PosterImageView(url: MovieAPI.imageURL + "/6WBeq4fCfn7AN0o21W9qNcRF2l9.jpg")
+                    .frame(width: 80)
+                Spacer()
+                    
+                VStack(alignment: .leading) {
+                    Divider().opacity(0)
+                    Text("AAAAAAA")
+                        .font(.notoSans(.Medium, size: 14))
+                    
+                    Text("⭐️ 9.2")
+                        .font(.notoSans(.Regular, size: 12))
+                    
+                    Text("1918년 제1차 세계 대전 말 뉴올리언즈. 80세의 외모를 가진 사내 아이가 태어난다. 그의 이름은 벤자민 버튼. 생김새때문에 양로원에 버려져 노인들과 함께")
+                        .font(.notoSans(.Regular, size: 12))
+                        .lineLimit(3)
+                    
+                    HStack {
+                        Spacer()
+                        Button {
+                            print("AA")
+                        } label: {
+                            Text("더 보기")
+                                .font(.notoSans(.Regular, size: 12))
+                                .foregroundColor(.accentColor)
+                        }
+                    }
+                   
+
+                }
+            }
         }
+        .listStyle(.plain)
         .searchable(text: $query)
     }
 }
