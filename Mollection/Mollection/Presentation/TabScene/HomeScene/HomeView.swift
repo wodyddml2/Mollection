f509efadfccddff0cbaef1c2e172bdcc36daf693
wodyddml2 <wodyddml4@naver.com>
2023-02-20 11:48:48 +0900

    Feat: HomeView Cell 클릭 시 DetailView 화면 전환, data 전달

--- Mollection/Mollection/Presentation/TabScene/HomeScene/HomeView.swift
+++ Mollection/Mollection/Presentation/TabScene/HomeScene/HomeView.swift
@@ -8,29 +8,27 @@
 import SwiftUI
 
 struct HomeView: View {
-    let a = [
-        MediaAPI.imageURL + "/91V6NI1JXCTDhaSSkoY7T87hk09.jpg",
-        MediaAPI.imageURL + "/zyzD9DGJWyA3VDMrS6ZzmW61SAd.jpg",
-        MediaAPI.imageURL + "/pGMfidaVkjMVHXNIl7yippnipFT.jpg",
-        MediaAPI.imageURL + "/6Ujbtp0NklUoQ67s32HyW6R5540.jpg"
-    ]
     @EnvironmentObject private var fbStore: FBStore
-  
+    
     private let columns: [GridItem] = Array(repeating: .init(.flexible(), spacing: 10), count: 3)
     
     var body: some View {
         ScrollView {
             LazyVGrid(columns: columns, spacing: 15) {
                 ForEach(fbStore.mediaInfos, id: \.id) { data in
-                    VStack {
-                        PosterImageView(url: data.mediaInfo.posterPath ?? "")
-                            .frame(height: 170)
-                        
-                        Text(data.mediaInfo.title ?? "")
-                            .font(.notoSans(.Regular, size: 12))
-                            .lineLimit(1)
+                    NavigationLink {
+                        DetailView(mediaData: data.mediaInfo)
+                    } label: {
+                        VStack {
+                            PosterImageView(url: data.mediaInfo.posterPath ?? "")
+                                .frame(height: 170)
+                            
+                            Text(data.mediaInfo.title ?? "")
+                                .font(.notoSans(.Regular, size: 12))
+                                .foregroundColor(.black)
+                                .lineLimit(1)
+                        }
                     }
-                    
                 }
             }
             .padding(10)
@@ -42,7 +40,7 @@ struct HomeView: View {
                 Image(systemName: "menucard")
             }
         }
-    
+        
     }
 }
 
